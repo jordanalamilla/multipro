dc17f7c518136332485dbfaa8b82a54f16ca6b7d
Jordan Alamilla <jordan.alamilla@gmail.com>
2019-01-20 19:36:42 -0500

    added cutom login logo

--- functions.php
+++ functions.php
@@ -2,6 +2,26 @@
 
 add_theme_support( 'post-thumbnails' );
 
+function my_login_logo() { ?>
+
+    <style type="text/css">
+
+        #login h1 a, .login h1 a {
+            
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
+            height:320px;
+            width:320px;
+            background-size: 320px 320px;
+            background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+
+    </style>
+
+<?php }
+
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
 function themename_custom_logo_setup() {
 
     $defaults = array(
